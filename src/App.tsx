2bae3da668cfa28b0bbfbff3fafcdcf46765ef7e
Jeongbin <yon980824@gmail.com>
2022-08-04 20:56:53 +0900

    Typescript - Forms, Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
 function App() {
+  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
+  const onClick2 = (event: React.MouseEvent<HTMLButtonElement>) => {};
   return (
-    <div>
-      <Circle borderColor="red" bgColor="teal" />
-      <Circle text="I'm here" bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>proptected</H1>
+      <Dummy text="hello" />
+
+      <form>
+        <button onClick={onClick}>click me</button>
+      </form>
+      <button onClick={onClick2}>click me</button>
+    </Container>
   );
 }
 
