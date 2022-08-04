59539c543512b9617d8c86929c5b66c1231529ea
Jeongbin <yon980824@gmail.com>
2022-08-04 16:48:01 +0900

    Typescript - DefinitelyTyped, Typing the props, Optional props, State

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="red" bgColor="teal" />
+      <Circle text="I'm here" bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
