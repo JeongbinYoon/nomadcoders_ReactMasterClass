f1a7634d63d099ead35455df2ce30309c01bb1c0
Jeongbin <yon980824@gmail.com>
2022-08-05 13:31:02 +0900

    Crypto tracker - Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-interface DummyProps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyProps) {
-  return <H1>{text}</H1>;
-}
 function App() {
-  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
-  const onClick2 = (event: React.MouseEvent<HTMLButtonElement>) => {};
-  return (
-    <Container>
-      <H1>proptected</H1>
-      <Dummy text="hello" />
-
-      <form>
-        <button onClick={onClick}>click me</button>
-      </form>
-      <button onClick={onClick2}>click me</button>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
